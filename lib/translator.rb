29a69d8d5b68736132ec4ab21d0e8862ef4920ef
Kyle Houghton <kylebenz@hotmail.com>
2019-01-02 21:58:51 +0000

    All tests passing.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,21 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  original_h = YAML.load_file(file_path)
+  new_h = {"get_emoticon" => {}, "get_meaning" => {}}
+  original_h.each do |meaning, emoticons|
+    new_h["get_emoticon"][emoticons[0]] = emoticons[1]
+    new_h["get_meaning"][emoticons[1]] = meaning
+  end
+  new_h
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)["get_emoticon"][emoticon]
+  result == nil ? "Sorry, that emoticon was not found" : result
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  result = load_library(file_path)["get_meaning"][emoticon]
+  result == nil ? "Sorry, that emoticon was not found" : result
 end
